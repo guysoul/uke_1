954f6b793dba68fbb22c2cccb10bf5e8401a00ba
Eduardo <cojr.eduardo@gmail.com>
2024-10-24 21:10:00 +0200

    Adding personal Info and header component

---
+++ online-cv/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import reactLogo from "../assets/react.svg";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Mitt Online CV</h1>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </div>
+  );
+};
+
+export default Header;
---
+++ online-cv/src/components/Personalinfo.jsx
@@ -0,0 +1,11 @@
+const PersonalInfo = () => {
+  return (
+    <div>
+      <p>Name : Eduardo Jr Co</p>
+      <p>Address : Oslo</p>
+      <p>Telephone : 90241694</p>
+    </div>
+  );
+};
+
+export default PersonalInfo;
